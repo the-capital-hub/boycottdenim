8aa8dd8b3a476b1cf4b386948b8e06a01e7d04d3
Hemu-77 <gowdasphemanth@gmail.com>
2025-07-21 18:02:20 +0530

    added databases

--- src/app/api/auth/verify-otp/route.ts
+++ src/app/api/auth/verify-otp/route.ts
@@ -1,49 +1,38 @@
 // <<<<<<< backend
 // app/api/verify-otp/route.ts
 import { NextResponse } from "next/server";
-import jwt from 'jsonwebtoken'
+import jwt from 'jsonwebtoken';
+import { connectDB } from "@/lib/dbconnect";
+import { User } from "@/models/User";
 
 
 const SECRET = process.env.JNW_SECRET || "supersecret123"
 
 
-// import connectDB from "@/utils/db";
+
 
 const dummyOtp = "123456"
 
 export async function POST(req: Request) {
-//   await connectDB();
-  const { otp } = await req.json();
+  await connectDB();
+  const { phone, otp } = await req.json();
 
-  if (!otp) {
+  if (!otp || !phone) {
     return NextResponse.json({ message: "Phone and OTP are required." }, { status: 400 });
   }
 
   if (dummyOtp !== otp)
     return NextResponse.json({ error: "Incorrect OTP" }, { status: 400 });
 
+  const user = await User.findOne({ phone });
 
-//   const token = jwt.sign(
-//     { userId: user._id, 
-//       phone: user.phone 
-//     }, 
-//     SECRET, 
-//     { expiresIn: "7d" });
 
-  return NextResponse.json({ message: "OTP verified successfully" });//token
-// =======
-// import { NextRequest, NextResponse } from "next/server";
+  const token = jwt.sign(
+    { userId: user._id, 
+      phone: user.phone 
+    }, 
+    SECRET, 
+    { expiresIn: "7d" });
 
-// const dummyOTP = "123456"; // Replace later with real OTP logic
-
-// export async function POST(req: NextRequest) {
-//   const {  otp } = await req.json();
-
-//   if (otp !== dummyOTP) {
-//     return NextResponse.json({ message: "Invalid OTP." }, { status: 401 });
-//   }
-
-//   // OTP is valid
-//   return NextResponse.json({ message: "OTP verified successfully!" });
-// >>>>>>> main
+  return NextResponse.json({ message: "OTP verified successfully" , token});//token
 }
