b34537f46aafbfb18bbb08a6bf3e125aaa172a05
Hemu-77 <gowdasphemanth@gmail.com>
2025-07-23 18:36:49 +0530

    Added product section

--- src/app/api/product/[id]/getProductById/route.ts
+++ src/app/api/product/[id]/getProductById/route.ts
@@ -1,25 +1,31 @@
+// src/app/api/product/[productId]/getProductById/route.ts
+
 import { NextRequest, NextResponse } from "next/server";
 import { Product } from "@/models/Products";
+import { connectDB } from "@/lib/dbconnect";
 
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  await connectDB();
 
-export async function GET( req : NextRequest, {params} : { params : { productId : String }}) {
-    try{
-        const { productId } = params
+  try {
+    const { id } = params;
 
-        if( !productId ){
-            return NextResponse.json({ message: "Product ID is required" }, { status: 400 });
-        }
+    if (!id) {
+      return NextResponse.json({ message: "Product ID is required" }, { status: 400 });
+    }
 
-        const product = await Product.findById(productId)
+    const product = await Product.findById(id);
 
-        if (!product) {
-            return NextResponse.json({ message: "Product not found" }, { status: 404 });
-        }
+    if (!product) {
+      return NextResponse.json({ message: "Product not found" }, { status: 404 });
+    }
 
-        return NextResponse.json({product}, { status: 201 })
-    }catch (error) {
-        console.error("Error finding product:", error);
-        return NextResponse.json({ message: "Internal Server Error" }, { status: 500 });
-      }
-    
+    return NextResponse.json({ product }, { status: 200 });
+  } catch (error) {
+    console.error("Error finding product:", error);
+    return NextResponse.json({ message: "Internal Server Error" }, { status: 500 });
+  }
 }
